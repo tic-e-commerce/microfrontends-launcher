c7fd141abc26ac6e1760b619a84317d381befcde
Ismael Toaquiza <ismaeltojair@gmail.com>
2025-01-18 12:18:04 -0500

    Add axios dependency and implement product service methods

---
+++ products-mf/services/products.service.ts
@@ -0,0 +1,30 @@
+import { Product } from "@/models";
+import axios from "axios";
+
+const API_URL =
+  process.env.NEXT_PUBLIC_BACKEND_URL || "http://localhost:4000/api/products";
+
+export const getProducts = async () => {
+  const response = await axios.get(`${API_URL}`);
+  return response.data;
+};
+
+export const getProductById = async (id: number) => {
+  const response = await axios.get(`${API_URL}/${id}`);
+  return response.data;
+};
+
+export const createProduct = async (productData: Product) => {
+  const response = await axios.post(`${API_URL}`, productData);
+  return response.data;
+};
+
+export const updateProduct = async (id: number, productData: Product) => {
+  const response = await axios.patch(`${API_URL}/${id}`, productData);
+  return response.data;
+};
+
+export const deleteProduct = async (id: number) => {
+  const response = await axios.delete(`${API_URL}/${id}`);
+  return response.data;
+};
