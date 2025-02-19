32cee8a9f9470a3828a7c904fe4a971a846c9b4d
Ismael Toaquiza <ismaeltojair@gmail.com>
2025-02-18 23:34:34 -0500

    Refactor docker-compose and remove unused services

--- products-mf/pages/index.tsx
+++ products-mf/pages/index.tsx
@@ -1,12 +1,10 @@
 import PaginatedProductsList from "@/components/paginated-products-list/paginated-products-list.component";
-import ProductDetail from "@/components/product-detail/product-detail.component";
 import ProductsList from "@/components/products-list/products-list.component";
 
 const Home = () => {
 
   return (
     <div>
-      <ProductDetail />
       <ProductsList title="Product List"/>
       <PaginatedProductsList title="Paginated Product List"/>
     </div>
